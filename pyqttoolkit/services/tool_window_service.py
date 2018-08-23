f64f313aa1d9a1f67a1b6428ded10608233b7bf6
Mike Tinning <mike@swyftech.com>
2018-08-23 15:44:05 +0100

    Ensure that tool window service objects stay in memory

--- pyqttoolkit/services/tool_window_service.py
+++ pyqttoolkit/services/tool_window_service.py
@@ -9,6 +9,7 @@ from pyqttoolkit.viewmanagers import ToolViewManager
 class ToolWindowService:
     def __init__(self, dependency_contianer):
         self._dependency_container = dependency_contianer
+        self._active_tools = []
     
     def open(self, parent, model, name):
         view = self._dependency_container.resolve_for(type(model), QWidget, {'parent': None})
@@ -19,5 +20,12 @@ class ToolWindowService:
 
         tool_window = ToolWindow(parent, view, name, show_buttons)
         view_manager = self._dependency_container.resolve_for(type(model), ToolViewManager, {'view': view, 'model': model})
-        tool_window.accepted.connect(view_manager.save)
+        tool_window.accepted.connect(self._tool_window_accepted(tool_window, view_manager))
+        self._active_tools.append((tool_window, view_manager))
         tool_window.show()
+
+    def _tool_window_accepted(self, tool_window, view_manager):
+        def _handler():
+            view_manager.save()
+            self._active_tools.remove((tool_window, view_manager))
+        return _handler
