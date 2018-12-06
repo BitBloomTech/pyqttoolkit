b0dcbeb8f09303c6e1c8f2b7396dcf785675e857
Mike Tinning <mike@swyftech.com>
2018-12-06 18:09:53 +0000

    Updating list view to reflect changes in selectedIndex

--- pyqttoolkit/views/list_view.py
+++ pyqttoolkit/views/list_view.py
@@ -7,6 +7,8 @@ from pyqttoolkit.properties import AutoProperty
 class ListView(QListView):
     def __init__(self, parent):
         QListView.__init__(self, parent)
+        self.selectedIndexChanged.connect(self._handle_selected_index_changed)
+        self._updating_selection = False
     
     selectedItemsChanged = pyqtSignal()
     selectedIndexChanged = pyqtSignal(int)
@@ -14,7 +16,13 @@ class ListView(QListView):
     selectedIndex = AutoProperty(int)
     
     def selectionChanged(self, current_selection, previous_selection):
+        self._updating_selection = True
         result = QListView.selectionChanged(self, current_selection, previous_selection)
         self.selectedIndex = current_selection.indexes()[0].row() if current_selection.indexes() else -1
         self.selectedItemsChanged.emit()
+        self._updating_selection = False
         return result
+
+    def _handle_selected_index_changed(self, index):
+        if self.model() is not None and not self._updating_selection:
+            self.setCurrentIndex(self.model().createIndex(index, 0))
