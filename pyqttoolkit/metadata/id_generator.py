7630ffe490b95a2af5b1a7f8290859bfbd1b32cb
Mike Tinning <mike@swyftech.com>
2019-01-04 14:06:54 +0000

    Fixing ID issue

--- pyqttoolkit/metadata/id_generator.py
+++ pyqttoolkit/metadata/id_generator.py
@@ -1,4 +1,4 @@
 def next_id(root, existing):
-    current_ids = sorted(i for i in existing if root in i)
-    last_id = int(current_ids[-1].replace(root, '')) if current_ids else 0
+    current_ids = sorted(int(i.replace(root, '')) for i in existing if root in i)
+    last_id = current_ids[-1] if current_ids else 0
     return f'{root}{last_id + 1}'
