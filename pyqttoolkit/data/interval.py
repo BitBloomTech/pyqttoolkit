cd2a6195f5e3192648a89687c181d9c4240a471c
NoePonceH <noe@bitbloom.tech>
2023-11-06 21:19:08 -0600

    Refactoring calculate_interval to use a bigger sample of data

--- pyqttoolkit/data/interval.py
+++ pyqttoolkit/data/interval.py
@@ -14,16 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+import pandas as pd
+
 def calculate_interval(values, sort=True):
-    interval = None
-    last_value = None
+    if len(values) < 2:
+        return None
     if sort:
         values = sorted(values)
-    for value in values:
-        if last_value is not None:
-            next_interval = value - last_value
-            if next_interval == interval:
-                return interval
-            interval = next_interval
-        last_value = value
-    return interval
+    values_series = pd.Series(values)
+    return (values_series - values_series.shift(1)).mode()[0]
