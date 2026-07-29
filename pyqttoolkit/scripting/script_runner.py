97fe276409e3d43a3ed2fd48ea19d4af3769281f
Michael Tinning <mike@bitbloom.tech>
2026-07-29 10:07:57 +0000

    Address review feedback

--- pyqttoolkit/scripting/script_runner.py
+++ pyqttoolkit/scripting/script_runner.py
@@ -73,9 +73,10 @@ class ScriptRunner:
             self._context = context
             with redirect_output(self._context, self._skip_empty_output_lines):
                 # Use one namespace so functions defined by the script can
-                # resolve each other recursively through their globals.
-                namespace = self._globals(context)
-                namespace.update(context.locals)
+                # resolve each other recursively through their globals. Add
+                # runner globals last so context locals cannot replace
+                # protected entries such as __builtins__.
+                namespace = {**context.locals, **self._globals(context)}
                 #pylint: disable=exec-used
                 exec(code, namespace, namespace)
                 #pylint: enable=exec-used
